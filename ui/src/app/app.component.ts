d864e101cd9b40172e72386ec109cd91f944debe
andreashauschild <andreas.hauschild.de@googlemail.com>
2024-03-17 13:09:09 +0100

    Updated to angular 15

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,15 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import SunsetTheme from 'highcharts/themes/dark-unica.js';
-import * as Highcharts from 'highcharts';
-import * as HighchartsStock from 'highcharts/highstock';
 import {BackendWebsocketService} from './shared/services/backend-websocket.service';
 import {Router} from '@angular/router';
 import {AuthenticationService} from './shared/services/authentication.service';
 import {SidebarLayoutModel} from './shared/ui/sidebar-layout/sidebar-layout.component';
 import {ApplicationService} from './shared/services/application.service';
 
-SunsetTheme(Highcharts);
-SunsetTheme(HighchartsStock);
 
 @Component({
   selector: 'app-root',
